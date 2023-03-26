7ab173844ebef4ff54b50d388f57850d58467701
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-03-26 14:51:41 -0300

    Ajax e função de captura e diferença da hora

--- public/index.php
+++ public/index.php
@@ -4,7 +4,25 @@
         print_r($array);
         echo "</pre>";
     }
+
+    function diffTime($horario_entrada, $horario_saida){
+
+        $diff = (strtotime($horario_saida) - strtotime($horario_entrada));
+
+        $horas =  floor($diff / 60 / 60);
+        $minutos = round(($diff - ($horas * 60 *60)) / 60);
+
+        $hora = str_pad($horas, 2, "0", STR_PAD_LEFT);
+        $minuto = str_pad($minutos, 2, "0", STR_PAD_LEFT);
+        
+        $horas_trabalhadas = "$hora:$minuto";
+        return $horas_trabalhadas;
+
+    }
     
     require_once"../vendor/autoload.php";
     $route = new \App\Route;
+
+    echo $horas_trabalhadas =  diffTime($_POST['horario_entrada'], $_POST['horario_saida']);
+    
 ?>
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,4 +1,26 @@
 var j = jQuery.noConflict(); // Utilizar o var invés do let para que seja aplicado de forma global
 j(document).ready(function () {
-    console.log("Teste")
+    console.log("Teste");
+    buttonEnviarHorario();
 })
+
+const buttonEnviarHorario = () => {
+    j('#enviarHorario').on('click', () => {
+        let horario_entrada = j('#horario_entrada').val();
+        let horario_saida = j('#horario_saida').val();
+        console.log(horario_entrada);
+        console.log(horario_saida);
+        j.ajax({
+            type: "POST",
+            url: "../index.php",
+            dataType: "HTML",
+            data: {
+                horario_entrada: horario_entrada,
+                horario_saida: horario_saida
+            },
+            success: (response) => {
+                console.log(response)
+            }
+        })
+    })
+}
